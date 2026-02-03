0c99a0a33a544aa29c5a7da1fed8eb5b642714d7
IngSaul004 <ing.sau.rivera004@.gmail.com>
2026-02-03 11:41:58 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import './App.css'
+import logoimg from './assets/logo.png'
+import Dashboard from './dashboard'
+import { useState } from 'react'
+
+function App() {
+  const [usuario, setUsuario] = useState('')
+  const [contrasena, setContrasena] = useState('')
+  const [logueado, setLogueado] = useState(false)
+
+  function inicioSesion(e) {
+    e.preventDefault() 
+
+    const usuarioValido = 'admin'
+    const contrasenaValida = 'admin123'
+
+    if (
+      usuario === usuarioValido &&
+      contrasena === contrasenaValida
+    ) {
+      setLogueado(true) 
+    } else {
+      alert('Usuario o contraseña incorrectos')
+    }
+  }
+
+  return (
+    <>
+      {logueado ? (
+        <Dashboard />
+      ) : (
+        <div className='inicioSesion'>
+          <img src={logoimg} alt="Logo" />
+          <h1>Salidas Almacen</h1>
+          <h2>Bienvenido</h2>
+
+          <form onSubmit={inicioSesion}>
+            <input
+              type="text"
+              placeholder="Usuario"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+
+            <input
+              type="password"
+              placeholder="Contraseña"
+              value={contrasena}
+              onChange={(e) => setContrasena(e.target.value)}
+            />
+
+            <button type="submit">Iniciar Sesión</button>
+          </form>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default App
